f571f197eb9598b0b2b16c60430542e39d854b84
pmrushi <rushi.pamu@zoomsystems.com>
2019-06-09 00:20:28 +0530

    Spring boot REST sample application setup

---
+++ src/main/java/com/pmr/rest/SprinbBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.pmr.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbBootRestApplication.class, args);
+	}
+
+}
